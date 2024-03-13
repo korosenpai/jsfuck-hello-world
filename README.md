a88395772216b42645a0ef9aa78399f73ec63159
koro <koro.dev404@gmail.com>
2024-03-13 12:43:22 +0100

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# hello world!
+
+never in my life ill do this again (almost 6 hours of agony)
---
+++ notes.js
@@ -0,0 +1,136 @@
+// logic //
+// get fill
+// use fill characters to get constructor
+// use constructor to get any ascii character 
+
+function sep() { console.log("\n++++++++++++++++++++++++++++++\n") }
+
+// fill
+console.log( (![]+[])[+![]] ) // f
+console.log( ([][[]]+[])[+!![] + +!![] + +!![] + +!![] + +!![]] ) // i
+console.log( (![]+[])[+!![] + +!![]] ) // l
+console.log( (![]+[])[+!![] + +!![]] ) // l
+
+console.log((![]+[])[+![]] + ([][[]]+[])[+!![] + +!![] + +!![] + +!![] + +!![]] + (![]+[])[+!![] + +!![]] + (![]+[])[+!![] + +!![]])
+
+sep()
+
+
+// constructor
+console.log( ([]["fill"] + [])[3] ) // c
+console.log( ([]["fill"] + [])[6] ) // o
+console.log( ([][[]] + [])[1] ) // n
+console.log( (![] + [])[3] ) // s
+console.log( (!![] + [])[0] ) // t
+console.log( (!![] + [])[1] ) // r
+console.log( (!![] + [])[2] ) // u
+console.log( ([]["fill"] + [])[3] ) // c
+console.log( (!![] + [])[0] ) // t
+console.log( ([]["fill"] + [])[6] ) // o
+console.log( (!![] + [])[1] ) // r
+
+sep()
+
+// console
+console.log( ([]["fill"] + [])[3] ) // c
+console.log( ([]["fill"] + [])[6] ) // o
+console.log( ([][[]] + [])[1] ) // n
+console.log( (![] + [])[3] ) // s
+console.log( ([]["fill"] + [])[6] ) // o
+console.log( (![]+[])[+!![] + +!![]] ) // l
+console.log( (!![]+[])[+!![] + +!![] + +!![]] ) // e
+
+// .
+console.log((+(+!+[]+[+!+[]]+(!![]+[])[!+[]+!+[]+!+[]]+[!+[]+!+[]]+[+[]])+[])[+!+[]])
+
+
+//log
+console.log( (![]+[])[+!![] + +!![]] ) // l
+console.log( ([]["fill"] + [])[6] ) // o
+console.log( (""["constructor"] + [])[14] ) // g
+
+
+sep()
+
+// S
+console.log( (""["constructor"] + [])[9] )
+
+sep()
+
+// 14 => 1 + 4
+console.log( (+!![] +[]) + (+!![] +!![] +!![] +!![] +[]) )
+
+sep()
+
+// "
+console.log(("")["fontcolor"]()[12])
+
+// (
+console.log( ([]["flat"]+"")[13] )
+// )
+console.log( ([0]+![]+[]["flat"])[20] )
+
+sep()
+
+// flat
+console.log( (![]+[])[+![]] ) // f
+console.log( (![]+[])[+!![] + +!![]] ) // l
+console.log( (![]+[])[+!![]] ) // a
+console.log( (!![] + [])[0] ) // t
+
+// 12
+console.log( (+!![] +[]) + (+!![] +!![] + []) )
+// 13
+console.log( (+!![] +[]) + (+!![] +!![] +!![] + []) )
+// 20
+console.log( (+!![] + +!![] +[]) + (+![] + []) )
+
+sep()
+
+// fontcolor
+console.log( (![]+[])[+![]] ) // f
+console.log( ([]["fill"] + [])[6] ) // o
+console.log( ([][[]] + [])[1] ) // n
+console.log( (!![] + [])[0] ) // t
+console.log( ([]["fill"] + [])[3] ) // c
+console.log( ([]["fill"] + [])[6] ) // o
+console.log( (![]+[])[+!![] + +!![]] ) // l
+console.log( ([]["fill"] + [])[6] ) // o
+console.log( (!![] + [])[1] ) // r
+
+// ;
+// ("")["fontcolor"](NaN+")[21]
+// Nan
+// +[![]]
+
+sep()
+
+// hello world
+console.log((+(101))["to"+String["name"]](21)[1]) // h
+console.log(([][[]] + [])[3]) // e
+console.log( (![]+[])[+!![] + +!![]] ) // l
+console.log( (![]+[])[+!![] + +!![]] ) // l
+console.log( ([]["fill"] + [])[6] ) // o
+
+console.log((NaN+[]["flat"])[11]) // " "
+
+console.log( (+(32))["to"+String["name"]](33) ) // w
+console.log( ([]["fill"] + [])[6] ) // o
+console.log( (!![] + [])[1] ) // r
+console.log( (![]+[])[+!![] + +!![]] ) // l
+console.log(([][[]] + [])[2]) // d
+
+// NaN
+console.log( +[![]] )
+
+// String
+console.log(([]+[])["constructor"])
+
+// Number
+console.log( (+[])["constructor"] )
+
+// name
+console.log( ([][[]] + [])[1] ) // n
+console.log( (![]+[])[+!![]] ) // a
+console.log( (Number+"")[11] ) // m
+console.log(([][[]] + [])[3]) // e
